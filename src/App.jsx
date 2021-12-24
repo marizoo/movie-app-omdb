6a75cceb34b8c98455dbc5efd19e26cece81a918
marizoo <marizoo@meow.woof>
2021-12-24 21:17:49 +0700

    added overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import MovieList from './components/MovieList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import MovieListHeading from './components/MovieListHeading'
+import SearchBox from './components/SearchBox'
 
-const DUMMY_DATA = [
-  {
-    "Title": "Star Wars: Episode IV - A New Hope",
-    "Year": "1977",
-    "imdbID": "tt0076759",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode V - The Empire Strikes Back",
-    "Year": "1980",
-    "imdbID": "tt0080684",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VI - Return of the Jedi",
-    "Year": "1983",
-    "imdbID": "tt0086190",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars: Episode VII - The Force Awakens",
-    "Year": "2015",
-    "imdbID": "tt2488496",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-},
-]
 
 const App = () => {
-  const [movies, setMovies] = useState(DUMMY_DATA)
+  const [movies, setMovies] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+
+  const getMovieRequest = async (searchValue) => {
+      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=9ac2fef7`
+      const response = await fetch(url);
+      const responseJson = await response.json()
+
+      // console.log(responseJson)
+      if (responseJson.Search) {
+        setMovies(responseJson.Search)
+      }
+  }
+
+  useEffect(() => {
+    getMovieRequest(searchValue)
+  },[searchValue])
 
   return (
-    <div>
-      <MovieList movies={movies} />
+    <div className='container-fluid movie-app'>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading='Movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+      <div className="row movie-wrapper" >
+          <MovieList movies={movies} />
+      </div>
     </div>
   )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,8 +4,9 @@ const MovieList = ({movies}) => {
     return (
         <>
             {movies.map((movie, index) => (
-                <div key={index}>
+                <div key={index} className='image-container d-flex justify-content-start m-3 movie-box'>
                     <img src={movie.Poster} alt='movie poster'></img>
+                    <div className='overlay d-flex align-items-center justify-content-center'></div>
                 </div>
             ))}
         </>
